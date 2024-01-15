2e9d8d427d7af6103f4fb1d38122db07387f1995
AMIT-PATILJIMS <ap1079997@gmail.com>
2024-01-15 17:16:29 +0530

    Add project files.

---
+++ ExceptionInDotNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExceptionInDotNet.MvcApplication" Language="C#" %>
---
+++ Logger/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace Logger
+{
+    public class Log : ILog
+    {
+        private static readonly Lazy<Log> instance = new Lazy<Log>(() => new Log());
+
+        public static Log GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        private Log()
+        {
+        }
+
+        public void LogException(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
